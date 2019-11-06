0d98d6f502bc9e5534fbb65494ff561255b281ef
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-06 22:24:35 +0100

    MVP completed

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,11 +3,11 @@ const db = require("../data/db-config");
 
 module.exports = {
   find,
-  findById
-  //   findSteps,
-  //   add,
-  //   update,
-  //   remove
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
 };
 
 function find() {
@@ -18,4 +18,29 @@ function findById(id) {
   return db("schemes")
     .where({ id })
     .first();
+}
+
+function findSteps(id) {
+  return db("steps as st")
+    .join("schemes as s", "st.scheme_id", "s.id")
+    .select("s.id", "s.scheme_name", "st.step_number")
+    .where({ scheme_id: id });
+}
+
+async function add(scheme) {
+  const [id] = await db("schemes").insert(scheme);
+
+  return findById(id);
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
 }
